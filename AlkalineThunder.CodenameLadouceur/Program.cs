6d4500d876c7bd7240072c568a755845cc84ca4c
Michael VanOverbeek <theplexgate@gmail.com>
2020-03-04 20:23:33 -0500

    initial commit

---
+++ AlkalineThunder.CodenameLadouceur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlkalineThunder.CodenameLadouceur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
