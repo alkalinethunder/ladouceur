f3089cc4052efd999555ec74ce95440a97966d7c
Michael VanOverbeek <theplexgate@gmail.com>
2020-03-09 11:57:11 -0400

    Add the dev console component.

--- AlkalineThunder.CodenameLadouceur/GameLoop.cs
+++ AlkalineThunder.CodenameLadouceur/GameLoop.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using AlkalineThunder.CodenameLadouceur.Input;
 using AlkalineThunder.CodenameLadouceur.Screens;
+using AlkalineThunder.DevConsole;
 
 namespace AlkalineThunder.CodenameLadouceur
 {
@@ -14,6 +15,8 @@ namespace AlkalineThunder.CodenameLadouceur
         private SpriteBatch _batch;
         private ScreenManager _screenManager = null;
         private InputManager _input = null;
+        
+        public DevConsole.DevConsole DevConsole { get; private set; }
 
         public GameLoop()
         {
@@ -26,21 +29,26 @@ namespace AlkalineThunder.CodenameLadouceur
             _graphics.PreferredBackBufferHeight = nativeResolution.Height;
 
             IsFixedTimeStep = true;
+
+            Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
+            _batch = new SpriteBatch(GraphicsDevice);
             _input = new InputManager(this);
             _screenManager = new ScreenManager(this);
             Components.Add(_input);
             Components.Add(_screenManager);
+
+            DevConsole = new DevConsole.DevConsole(this, new SpriteBatchConsoleRenderer(_batch));
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _batch = new SpriteBatch(GraphicsDevice);
+            this.DevConsole.ConsoleFont = Content.Load<SpriteFont>("ConsoleFont");
 
             base.LoadContent();
         }
