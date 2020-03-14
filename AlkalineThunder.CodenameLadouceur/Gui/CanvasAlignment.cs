d19682c5c23a94bd7683871617b00d4539d045cb
Michael VanOverbeek <theplexgate@gmail.com>
2020-03-14 17:23:28 -0400

    Start work on the fucking canvas panel.

---
+++ AlkalineThunder.CodenameLadouceur/Gui/CanvasAlignment.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace AlkalineThunder.CodenameLadouceur.Gui
+{
+    public struct CanvasAlignment
+    {
+        public float LeftValue;
+        public float TopValue;
+
+        public CanvasAlignment(float left, float top)
+        {
+            LeftValue = left;
+            TopValue = top;
+        }
+
+        public static implicit operator CanvasAlignment(Vector2 vector)
+        {
+            return new CanvasAlignment(vector.X, vector.Y);
+        }
+
+        public static bool operator ==(CanvasAlignment a, CanvasAlignment b)
+        {
+            return (a.LeftValue == b.LeftValue) && (a.TopValue == b.TopValue);
+        }
+
+        public static bool operator !=(CanvasAlignment a, CanvasAlignment b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CanvasAlignment ca && ca == this;
+        }
+
+        public override string ToString()
+        {
+            return $"(Left={LeftValue}, Top={TopValue})";
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(LeftValue, TopValue);
+        }
+
+        public static CanvasAlignment TopLeft => new CanvasAlignment(0, 0);
+        public static CanvasAlignment Top => new CanvasAlignment(0.5f, 0);
+        public static CanvasAlignment TopRight => new CanvasAlignment(1, 0);
+        public static CanvasAlignment Left => new CanvasAlignment(0, 0.5f);
+        public static CanvasAlignment Center => new CanvasAlignment(0.5f, 0.5f);
+        public static CanvasAlignment Right => new CanvasAlignment(1, 0.5f);
+        public static CanvasAlignment BottomLeft => new CanvasAlignment(0, 1);
+        public static CanvasAlignment Bottom => new CanvasAlignment(0.5f, 1);
+        public static CanvasAlignment BottomRight => new CanvasAlignment(1, 1);
+    }
+}
---
+++ AlkalineThunder.CodenameLadouceur/Gui/CanvasPanel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlkalineThunder.CodenameLadouceur.Gui
+{
+    public sealed class CanvasPanel : LayoutControl
+    {
+        protected override Vector2 MeasureOverride()
+        {
+            return Vector2.Zero;
+        }
+    }
+}
