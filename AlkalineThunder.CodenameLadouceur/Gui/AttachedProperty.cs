7d7c4a9bdfe29486fc9ac0dce8ec13104fe08a88
Michael VanOverbeek <theplexgate@gmail.com>
2020-03-14 17:24:28 -0400

    Attached properties...

---
+++ AlkalineThunder.CodenameLadouceur/Gui/AttachedProperty.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlkalineThunder.CodenameLadouceur.Gui
+{
+    /// <summary>
+    /// Represents the structure of an attached property.
+    /// </summary>
+    public interface IAttachedProperty
+    {
+        /// <summary>
+        /// Gets the name of the attached property.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gets the value stored in the attached property.
+        /// </summary>
+        public object Value { get; }
+    }
+
+    /// <summary>
+    /// Represents a generic <see cref="IAttachedProperty"/> used to store values of a specific type.
+    /// </summary>
+    /// <typeparam name="T">The type of data held inside this attached property.</typeparam>
+    public class AttachedProperty<T> : IAttachedProperty
+    {
+        private T _value;
+
+        /// <summary>
+        /// Gets the name of the attached property.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gets the value stored inside the attached property.
+        /// </summary>
+        object IAttachedProperty.Value => _value;
+
+        /// <summary>
+        /// Gets the value stored inside the attached property.
+        /// </summary>
+        public T Value => _value;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="AttachedProperty{T}"/> class.
+        /// </summary>
+        /// <param name="name">The name of the attached property.</param>
+        /// <param name="value">The value of the attached property.</param>
+        public AttachedProperty(string name, T value)
+        {
+            Name = name;
+            _value = value;
+        }
+
+        /// <summary>
+        /// Sets the value of the attached property.
+        /// </summary>
+        /// <param name="value">The new value to store in the property.</param>
+        public void SetValue(T value)
+        {
+            _value = value;
+        }
+    }
+}
