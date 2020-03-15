ccdf88673b75f57d156c0727342f90073137dc86
Michael VanOverbeek <theplexgate@gmail.com>
2020-03-14 23:05:39 -0400

    Add some buttons to the about UI

--- AlkalineThunder.CodenameLadouceur/Screens/AboutScreen.cs
+++ AlkalineThunder.CodenameLadouceur/Screens/AboutScreen.cs
@@ -11,7 +11,11 @@ namespace AlkalineThunder.CodenameLadouceur.Screens
     {
         private StackPanel _infoStacker = new StackPanel();
         private Label _nucleusText = new Label("Nucleus\r\nUI Test Screen");
-        
+        private StackPanel _buttons = new StackPanel();
+        private Button _github = new Button();
+        private Button _exit = new Button();
+
+
         protected override void OnInitialize()
         {
             var canvas = new CanvasPanel();
@@ -19,11 +23,30 @@ namespace AlkalineThunder.CodenameLadouceur.Screens
             canvas.Margin = 25;
 
             canvas.Children.Add(_infoStacker);
+            canvas.Children.Add(_buttons);
+
+            _buttons.Orientation = Orientation.Horizontal;
+            _buttons.Spacing = 15;
+
+            _buttons.Children.Add(_github);
+            _buttons.Children.Add(_exit);
+
+            _github.Content = new Label("Source code");
+            _exit.Content = new Label("Exit");
+
+            _github.Content.HorizontalAlignment = HorizontalAlignment.Center;
+            _github.Content.VerticalAlignment = VerticalAlignment.Middle;
+
+            _exit.Content.HorizontalAlignment = HorizontalAlignment.Center;
+            _exit.Content.VerticalAlignment = VerticalAlignment.Middle;
 
             _infoStacker.Children.Add(_nucleusText);
 
             canvas.SetAnchor(_infoStacker, CanvasAlignment.BottomLeft);
             canvas.SetAlignment(_infoStacker, CanvasAlignment.BottomLeft);
+
+            canvas.SetAnchor(_buttons, CanvasAlignment.BottomRight);
+            canvas.SetAlignment(_buttons, CanvasAlignment.BottomRight);
 
             Content = canvas;
             base.OnInitialize();
